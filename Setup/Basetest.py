d83b0eee970b6856b40784fc26aa0af7d3a29429
Manisha Gharti <ghartimaneesa@gmail.com>
2025-03-06 18:54:57 +0545

    Changes in Configuration setup

--- Setup/Basetest.py
+++ Setup/Basetest.py
@@ -9,19 +9,19 @@ class BaseTest:
     logger = logging.getLogger(__name__)
 
     def setup_method(self, method):
-        driver_path = r"C:\Users\manishag\OneDrive - Infinite Computer Solutions (India) Limited\Documents\AutomationWithSelenium\Bin\chromedriver.exe"
-
-        ser = Service(driver_path)
-        logging.info("set up driver")
-        driver = webdriver.Chrome(service=ser)
-        # self.driver = webdriver.Chrome()
-        self.driver = driver
+        # driver_path = r"C:\Users\manishag\OneDrive - Infinite Computer Solutions (India) Limited\Documents\AutomationWithSelenium\Bin\chromedriver.exe"
+        #
+        # ser = Service(driver_path)
+        # logging.info("set up driver")
+        # driver = webdriver.Chrome(service=ser)
+        self.driver = webdriver.Chrome()
+        # self.driver = driver
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
 
-        # creds_path = r"C:\Users\user\Documents\PYTHONPROJECTS\python_automation_nidwija_trainee2025\cred\creds.json"
-        # with open(creds_path, "r") as f:
-        #     self.creds = json.load(f)
+        creds_path = r"C:\Users\manishag\OneDrive - Infinite Computer Solutions (India) Limited\Documents\AutomationWithSelenium\Credentials\creds.json"
+        with open(creds_path, "r") as f:
+            self.creds = json.load(f)
 
     def teardown_method(self, method):
         self.driver.quit()
